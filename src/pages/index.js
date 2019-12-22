a068450f7c6c544efed80aea2f2c1f3c03f66166
DaichiTakigawa <1023daichi@gmail.com>
2019-12-22 21:01:34 +0900

    header情報を追加。

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import React from "react"
 import Content from "../components/organisms/Content"
 import App from "../components/organisms/App"
 import Home from "../components/molecules/Home"
+import Seo from "../components/atoms/Seo"
 
 
 export default () => (
   <App>
+    <Seo title="たきがわのメモ" description="備忘録"/>
     <Content>
       <Home>
 
