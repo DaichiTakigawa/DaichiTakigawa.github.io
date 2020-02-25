ebc6ae2620e5b1a02064baab9ff45793d6f2ac31
DaichiTakigawa <1023daichi@gmail.com>
2020-02-25 18:08:03 +0900

    auto udpate.

--- src/components/molecules/Navigation.js
+++ src/components/molecules/Navigation.js
@@ -69,6 +69,19 @@ export default class Navigation extends React.Component {
           <StyledDivider />
           {this.getLinkList()}
           {this.getSNSLink()}
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
+          <ins
+            class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-6195920683902846"
+            data-ad-slot="4511974705"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
         </StyledSticky>
       </>
     )
