99627ca3746f417282828f612a5900f3d106b2d5
DaichiTakigawa <1023daichi@gmail.com>
2020-02-26 12:36:11 +0900

    auto udpate.

--- src/components/atoms/Adsense.js
+++ src/components/atoms/Adsense.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import NoSSR from "react-no-ssr"
+import Adsense from "react-adsense"
 
 import { rhythm } from "../../utils/typography"
 import { responsive } from "../../../config"
@@ -9,16 +9,20 @@ export default class Adsense extends React.Component {
   render() {
     let format = this.props.format || "auto"
     return (
-      <NoSSR>
-        <Container>
-          <Ins
-            className="adsbygoogle"
-            data-ad-client="ca-pub-6195920683902846"
-            data-ad-slot="4511974705"
-            data-ad-format={format}
-          />
-        </Container>
-      </NoSSR>
+      <Container>
+        <AdSense.Google
+          client="ca-pub-7292810486004926"
+          slot="4511974705"
+          style={{ display: "block" }}
+          format={format}
+        />
+        {/* <Ins
+          className="adsbygoogle"
+          data-ad-client="ca-pub-6195920683902846"
+          data-ad-slot="4511974705"
+          data-ad-format={format}
+        /> */}
+      </Container>
     )
   }
 }
