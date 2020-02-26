69bce1841a7b687a0629fd8f685c4b23124e633b
DaichiTakigawa <1023daichi@gmail.com>
2020-02-26 12:56:09 +0900

    auto udpate.

--- src/components/atoms/Adsense.js
+++ src/components/atoms/Adsense.js
@@ -7,16 +7,20 @@ import { responsive } from "../../../config"
 export default class Adsense extends React.Component {
   render() {
     let format = this.props.format || "auto"
-    return (
-      <Container>
-        <Ins
-          className="adsbygoogle"
-          data-ad-client="ca-pub-6195920683902846"
-          data-ad-slot="4511974705"
-          data-ad-format={format}
-        />
-      </Container>
-    )
+    if (!window || window.innerWidth >= responsive.tablet.minWidth) {
+      return (
+        <Container>
+          <Ins
+            className="adsbygoogle"
+            data-ad-client="ca-pub-6195920683902846"
+            data-ad-slot="4511974705"
+            data-ad-format={format}
+          />
+        </Container>
+      )
+    } else {
+      return null
+    }
   }
 }
 
@@ -25,9 +29,11 @@ const Container = styled.div`
 `
 
 const Ins = styled.ins`
-  display: none;
+  // display: none;
 
-  @media (min-width: ${responsive.tablet.minWidth}) {
-    display: block;
-  }
+  // @media (min-width: ${responsive.tablet.minWidth}) {
+  //   display: block;
+  // }
+
+  display: block;
 `
