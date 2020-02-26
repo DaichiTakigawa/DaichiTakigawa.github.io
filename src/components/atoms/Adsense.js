49ca09edff47ccd8887747e6f5fe47f253e614d3
DaichiTakigawa <1023daichi@gmail.com>
2020-02-26 12:45:30 +0900

    auto udpate.

--- src/components/atoms/Adsense.js
+++ src/components/atoms/Adsense.js
@@ -1,6 +1,5 @@
 import React from "react"
 import styled from "styled-components"
-import AdSense from "react-adsense"
 
 import { rhythm } from "../../utils/typography"
 import { responsive } from "../../../config"
@@ -10,18 +9,12 @@ export default class Adsense extends React.Component {
     let format = this.props.format || "auto"
     return (
       <Container>
-        <AdSense.Google
-          client="ca-pub-7292810486004926"
-          slot="4511974705"
-          style={{ display: "block" }}
-          format={format}
-        />
-        {/* <Ins
+        <Ins
           className="adsbygoogle"
           data-ad-client="ca-pub-6195920683902846"
           data-ad-slot="4511974705"
           data-ad-format={format}
-        /> */}
+        />
       </Container>
     )
   }
