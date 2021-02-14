7d854581663008c528a3d6bf8edcdc2654af8aa0
DaichiTakigawa <1023daichi@gmail.com>
2021-02-14 16:24:08 +0900

    auto udpate.

---
+++ src/components/molecules/navigation/LinkList.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import {Link} from 'gatsby';
+import styled from '@emotion/styled';
+import {rhythm} from '../../../lib/typography';
+import {UiContext} from '../../../contexts';
+
+const LinkList: React.FC = () => {
+  const {slug} = React.useContext(UiContext.Context);
+
+  return (
+    <Menu className={'manu'}>
+      <ul className={'menu-list'}>
+        <li>
+          <Link
+            className={slug === UiContext.TopPages.HOME ? 'is-active' : ''}
+            to={'/'}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={slug === UiContext.TopPages.BLOG ? 'is-active' : ''}
+            to={'/blog/'}>
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={slug === UiContext.TopPages.ABOUT ? 'is-active' : ''}
+            to={'/about/'}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={slug === UiContext.TopPages.CONTACT ? 'is-active' : ''}
+            to={'/contact/'}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </Menu>
+  );
+};
+
+const Menu = styled.nav({
+  margin: `${rhythm(2)} ${rhythm(1)}`,
+});
+
+export default LinkList;
