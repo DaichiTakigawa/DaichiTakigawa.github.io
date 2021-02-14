e43283ba9494458eb21e02e674c92cd5d494d668
DaichiTakigawa <1023daichi@gmail.com>
2021-02-14 17:05:57 +0900

    auto udpate.

--- src/domain/models/user.ts
+++ src/domain/models/user.ts
@@ -8,6 +8,6 @@ export interface Model {
 export async function login(): Promise<Model> {
   const response = await api.get('/login');
   if (200 <= response.status && response.status < 300) {
-    return response.data;
+    return response.data['user'];
   }
 }
